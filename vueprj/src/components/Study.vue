1dddf7663e8762ebceff5974ef87882ffca84a70
0245000055 <0245000055@zte.com.cn>
2018-04-06 15:39:51 +0800

    vue init

---
+++ vueprj/src/components/Study.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      {{msg}}
+      <p v-html="userName" />
+      <span v-bind:class="vbclass">CSS</span>
+    <br/>
+      <span v-bind:title="vbtitle">TITLE</span>
+    <br/>
+      {{msg.split('').join(',')}}
+    <br/>
+    <p v-if="showflg">TRUE</p>
+    <p v-else>FALSE</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+          msg:'用户登录',
+          userName : '用户名：',
+          vbclass : 'c1',
+          vbtitle : '动态',
+          showflg : false
+      }
+  }
+}
+</script>
+
+<style>
+c1{
+    color: red
+}
+</style>
+
+
