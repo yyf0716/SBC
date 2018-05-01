32cd9881843cf01608fddf53e4b402eed99c4518
0245000055 <0245000055@zte.com.cn>
2018-05-01 10:18:16 +0800

    学习

--- sbc-member/src/main/java/com/sbc/config/DruidConfig.java
+++ sbc-member/src/main/java/com/sbc/config/DruidConfig.java
@@ -1,5 +1,59 @@
 package com.sbc.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+
+@Configuration
 public class DruidConfig {
 
+	@ConfigurationProperties(prefix = "spring.datasource")
+	@Bean
+	public DataSource druid() {
+		return new DruidDataSource();
+	}
+
+	// 后台管理的Servlet
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		ServletRegistrationBean bean = new ServletRegistrationBean();
+		bean.setServlet(new StatViewServlet());
+
+		bean.addUrlMappings("/druid/*");
+
+		Map<String, String> initParameters = new HashMap<String, String>();
+		initParameters.put("resetEnable", "false"); // 禁用HTML页面上的“Rest All”功能
+		initParameters.put("loginUsername", "admin"); // ++监控页面登录用户名
+		initParameters.put("loginPassword", "admin"); // ++监控页面登录用户密码
+		initParameters.put("deny", ""); // ip黑名单
+		initParameters.put("allow", ""); // ip白名单（没有配置或者为空，则允许所有访问）
+
+		// 如果某个ip同时存在，deny优先于allow
+		bean.setInitParameters(initParameters);
+
+		return bean;
+	}
+
+	// 监控FILTER
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean bean = new FilterRegistrationBean();
+		bean.setFilter(new WebStatFilter());
+		// bean.addUrlPatterns("/*");
+		// bean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico"); //
+		// 不过滤
+		// bean.addInitParameter("profileEnable", "true");
+		return bean;
+	}
 }
--- vueprj/src/App.vue
+++ vueprj/src/App.vue
@@ -1,40 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-
-    <Study/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld'
-import Login from './components/Login'
-import Study from './components/Study'
-
-
-
-
-
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Login,
-    Study
-  }
+  name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
